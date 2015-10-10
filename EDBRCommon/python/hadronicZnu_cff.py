405c9e8fe3c65ec10a5d73971a51a2392a6ec891
David Romero Abad <dromeroa@cern.ch>
2015-10-10 17:25:19 +0200

    add cut for the Z mass

--- EDBRCommon/python/hadronicZnu_cff.py
+++ EDBRCommon/python/hadronicZnu_cff.py
@@ -1,11 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
-### FOR NOW : no cut in jet mass (In the plotter we will define the cuts (blinded))
+### This add the payload to the jets in order to use later the JEC  
+### Then we apply a cut in the mass of the fatjets in order to select the graviton in the event with the right mass
 
-hadronicVnu = cms.EDFilter( "CandViewSelector",
-                              src = cms.InputTag("niceJets"),
-                              cut = cms.string('pt > 200. '), ## Consider a Boosted Z
-                              filter = cms.bool(True) )
 
-hadronicVnuSequence = cms.Sequence(hadronicVnu)
+corrJetsProducer = cms.EDProducer ( "CorrJetsProducer",
+                                     jets     =    cms.InputTag( "niceJets" ),
+                                     vertex   =    cms.InputTag( "goodOfflinePrimaryVertex" ),
+                                     rho      =    cms.InputTag( "fixedGridRhoFastjetAll" ),
+                                     payload  =    cms.string  ( "AK8PFchs" ),
+                                     isData   =    cms.bool    ( False ))
+
+
+hadronicVnu = cms.EDFilter(  "CandViewSelector",
+                               src      =      cms.InputTag("corrJetsProducer:corrJets"),
+                               cut      =      cms.string('pt > 200. & '
+                                                          'userFloat("ak8PFJetsCHSCorrPrunedMass") > 40. & '
+                                                          'userFloat("ak8PFJetsCHSCorrPrunedMass") < 105.  '),
+                               filter   =      cms.bool(True) )
+
+
+hadronicVnuSequence = cms.Sequence( corrJetsProducer   *
+                                    hadronicVnu        )
 
