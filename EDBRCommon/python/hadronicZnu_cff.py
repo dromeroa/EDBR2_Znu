d91683eae817e7525679694a2be5d26c9ccd09ac
David Romero Abad <dromeroa@cern.ch>
2015-10-11 16:56:18 +0200

    define a proceess in a cfg

--- EDBRCommon/python/hadronicZnu_cff.py
+++ EDBRCommon/python/hadronicZnu_cff.py
@@ -1,5 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
+###--------------------------------------------------------------------------------------------
+### IF WE NEED TO LOAD SOME CONFIGURATION FILES, WE CAN DEFINE A PROCESS
+###
+###  def hadroniZnu(process):
+###
+###     ### Geometry and Detector Conditions (needed for a few patTuple production steps)
+###     process.load('Configuration.Geometry.GeometryIdeal_cff')
+###
+###     etc,...
+###
+###     LOOK INTO https://github.com/cms-jet/JMEDAS/blob/master/python/run2muoniso_cff.py
+###---------------------------------------------------------------------------------------------
+
 ### This add the payload to the jets in order to use later the JEC  
 ### Then we apply a cut in the mass of the fatjets in order to select the graviton in the event with the right mass
 
