eaa298c87b9943b97a460042079c27679430c49b
David Romero Abad <dromeroa@cern.ch>
2015-11-25 18:08:18 +0100

    add single electron trigger

--- EDBRCommon/python/hltFilterZnu_cff.py
+++ EDBRCommon/python/hltFilterZnu_cff.py
@@ -8,7 +8,10 @@ hltFilterZnu = hlt.triggerResultsFilter.clone(
 ### In 25 ns we have to change the NOISE cleaned in the path
 #triggerConditions = (  'HLT_PFMETNoMu90_NoiseCleaned_PFMHTNoMu90_IDTight_v*', 'HLT_MonoCentralPFJet80_PFMETNoMu120_NoiseCleaned_PFMHTNoMu120_IDTight_v*' ),
 #triggerConditions = (  'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*', 'HLT_MonoCentralPFJet80_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight_v*' ),
+## FOR MET DATA
 triggerConditions = (  'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*',),
+## FOR SINGLE ELECTRON DATA
+#triggerConditions = (  'HLT_Ele23 WPLoose Gsf_v*',),
                        hltResults = cms.InputTag( "TriggerResults", "", "HLT"),
                        l1tResults = cms.InputTag( "gtDigis" ),
                        throw = False )
