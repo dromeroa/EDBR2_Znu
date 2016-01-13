8ef4b6b6b7af3dde26813bfbd841c2b0a6987702
David Romero Abad <dromeroa@cern.ch>
2016-01-13 06:44:16 +0100

    new config files

---
+++ EDBRTreeMaker/run2015D/crabConfig_EDBR_analysis_MET_Run2015D_05Oct2015_BASELINE.py
@@ -0,0 +1,30 @@
+from CRABClient.UserUtilities import config
+config = config()
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.General.requestName = 'EDBR_MET_2015D_25ns_05Oct2015'
+
+config.General.workArea = 'EDBR_crab_projects'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'analysis-MET_Run2015D_25ns_13Ene_05Oct2015_BASELINE.py'
+
+
+
+config.section_('Data')
+config.Data.inputDataset= '/MET/Run2015D-05Oct2015-v1/MINIAOD'
+config.Data.unitsPerJob = 200
+config.Data.splitting = 'LumiBased'
+config.Data.lumiMask = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
+config.Data.publication = False
+config.Data.useParent = False
+
+
+config.Data.publication = False
+## User options
+config.section_("User")
+#config.User.email = 'dromeroa@cern.ch'
+## Site options
+config.section_("Site")
+config.Site.storageSite = 'T2_BR_SPRACE'
