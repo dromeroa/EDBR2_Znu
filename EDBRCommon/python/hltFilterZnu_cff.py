9ba0626fe124aba28470a44bb21befae4ab4012e
David Romero Abad <dromeroa@cern.ch>
2016-03-17 11:31:39 +0100

    edit hlt

--- EDBRCommon/python/hltFilterZnu_cff.py
+++ EDBRCommon/python/hltFilterZnu_cff.py
@@ -1,13 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
-### Note that we can use any version adding _v* in each path
 
 hltFilterZnu = hlt.triggerResultsFilter.clone(
 
-# Check if this work for MC and Data?
 
-## FOR MET DATA AND MC
+## DATA AND MC
 triggerConditions = (  'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*',),
                        hltResults = cms.InputTag( "TriggerResults", "", "HLT"),
                        l1tResults = cms.InputTag( "gtDigis" ),
