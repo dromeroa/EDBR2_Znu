2ebe89397bb7d85bf4c7908462d2e5d0ebda668f
David Romero Abad <dromeroa@cern.ch>
2016-03-18 06:00:24 +0100

    fix bug ak4jets

--- EDBRCommon/python/deltaPhiak4Jets_cff.py
+++ EDBRCommon/python/deltaPhiak4Jets_cff.py
@@ -4,8 +4,9 @@ import FWCore.ParameterSet.Config as cms
 
 
 filterQCD = cms.EDFilter("QCDfilter",
-                            jets = cms.InputTag("selectak4Jets"),
-                            deltaphi_threshold   = cms.double(2.8)
+                            jets = cms.InputTag("niceak4Jets"),
+                            deltaphi_threshold   = cms.double(2.8),
+                            filter = cms.bool(True)
 )
 
 deltaPhiak4JetsSequence = cms.Sequence( filterQCD )
