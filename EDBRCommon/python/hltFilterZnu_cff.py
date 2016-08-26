63f9facda7a1d348c2a4d75e72fa73afe5fda04c
David Romero Abad <dromeroa@cern.ch>
2016-08-26 02:10:19 +0200

    or trigger on 76X

--- EDBRCommon/python/hltFilterZnu_cff.py
+++ EDBRCommon/python/hltFilterZnu_cff.py
@@ -8,7 +8,7 @@ hltFilterZnu = hlt.triggerResultsFilter.clone(
 # Check if this work for MC and Data?
 
 ## FOR MET DATA AND MC
-triggerConditions = (  'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*',),
+triggerConditions = ( 'HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*','HLT_PFMET170*'),
                        hltResults = cms.InputTag( "TriggerResults", "", "HLT"),
                        l1tResults = cms.InputTag( "gtDigis" ),
                        throw = False )
